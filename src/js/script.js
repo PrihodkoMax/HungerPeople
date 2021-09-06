43dcec1c2c370086bcefb94390b1f8d55fc9b5cf
Maxim <1986maxim1508@gmail.com>
2021-09-06 17:09:19 +0300

    Start project

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+// window.addEventListener('DOMContentLoaded', function () {
+
+// 	// Menu back panel
+
+// 	const backdrop = document.querySelector('.backdrop');
+// 	const menuButton = document.querySelector('.header__logo img');
+// 	const menuCloseButton = document.querySelector('.menu__close');
+// 	const menuPanel = document.querySelector('menu.menu');
+
+// 	menuButton.addEventListener('click', () => {
+// 		showMenu();
+// 	});
+
+// 	menuCloseButton.addEventListener('click', () => {
+// 		closeMenu();
+// 	});
+
+// 	backdrop.addEventListener('click', () => {
+// 		closeMenu();
+// 	});
+
+// 	function showMenu() {
+// 		menuPanel.classList.add('menu_show');
+// 		backdrop.classList.add('opened');
+// 		document.body.style.overflow = 'hidden';
+// 	}
+
+// 	function closeMenu() {
+// 		menuPanel.classList.remove('menu_show');
+// 		backdrop.classList.remove('opened');
+// 		document.querySelector('body').style.overflow = 'auto';
+// 	}
+// });
