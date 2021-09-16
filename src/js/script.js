a2560aa45aa5d7a9666dd9ea1cee6f4d4e955522
Maxim <1986maxim1508@gmail.com>
2021-09-16 13:02:26 +0300

    End web layout and start mobile

--- src/js/script.js
+++ src/js/script.js
@@ -31,3 +31,25 @@
 // 		document.querySelector('body').style.overflow = 'auto';
 // 	}
 // });
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+	const menu = document.querySelector('.menumob'),
+		menuItem = document.querySelectorAll('.menu_item'),
+		hamburger = document.querySelector('.menu_button');
+
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		//  menu.classList.toggle('menu_active');
+	});
+
+	// menuItem.forEach(item => {
+	// 	 item.addEventListener('click', () => {
+	// 		  hamburger.classList.toggle('hamburger_active');
+	// 		  menu.classList.toggle('menu_active');
+	// 	 })
+	// })
+})
+
+
